0cf5a187662fc3e95779f7a5ce2004088388abd8
kirillColeBou <kirillteplycov888@gmail.com>
2024-12-13 18:59:30 +0500

    Добавьте файлы проекта.

---
+++ pr7_Тепляков/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr7_Тепляков
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
