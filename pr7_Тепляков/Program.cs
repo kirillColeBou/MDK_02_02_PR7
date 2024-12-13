e61cdb1c1781fa2bec9f270dad68d67e5ae7a475
kirillColeBou <kirillteplycov888@gmail.com>
2024-12-13 19:36:57 +0500

    Реализовано на 3. Тепляков

--- pr7_Тепляков/Program.cs
+++ pr7_Тепляков/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -13,42 +15,82 @@ namespace pr7_Тепляков
     {
         static void Main(string[] args)
         {
-            SingIn("", "");
+            try
+            {
+                using (StreamWriter file = new StreamWriter("debug.log", true))
+                {
+                    Trace.Listeners.Add(new TextWriterTraceListener(file));
+                    Trace.AutoFlush = true;
+                    SingIn("student", "Asdfg123");
+                    Cookie tokenCookie = new Cookie("token", "your_token_here") { Domain = "news.permaviat.ru" };
+                    GetToken(tokenCookie);
+                    string htmlCode = GetHtmlFromUrl("http://news.permaviat.ru/main");
+                    ParsingHtml(htmlCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                Trace.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
             Console.Read();
         }
 
         public static void SingIn(string Login, string Password)
         {
-            string url = "";
-            Debug.WriteLine($"Выполняем запрос: {url}");
+            string url = "http://news.permaviat.ru/ajax/login.php";
+            Trace.WriteLine($"Выполняем запрос: {url}");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
-            request.ContentType = "";
+            request.ContentType = "application/x-www-form-urlencoded";
             request.CookieContainer = new CookieContainer();
-            string postData = "";
+            string postData = $"login={Login}&password={Password}";
             byte[] Data = Encoding.ASCII.GetBytes(postData);
             request.ContentLength = Data.Length;
-            using(var stream = request.GetRequestStream())
+            using (var stream = request.GetRequestStream())
             {
                 stream.Write(Data, 0, Data.Length);
             }
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+            Trace.WriteLine($"Статус выполнения: {response.StatusCode}");
             string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
             Console.WriteLine(responseFromServer);
         }
 
         public static void GetToken(Cookie Token)
         {
-            string url = "";
-            Debug.WriteLine($"Выполняем запрос: {url}");
+            string url = "http://news.permaviat.ru/main";
+            Trace.WriteLine($"Выполняем запрос: {url}");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.CookieContainer = new CookieContainer();
             request.CookieContainer.Add(Token);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+            Trace.WriteLine($"Статус выполнения: {response.StatusCode}");
             string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
             Console.WriteLine(responseFromServer);
+        }
+
+        public static void ParsingHtml(string htmlCode)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(htmlCode);
+            var Document = html.DocumentNode;
+            IEnumerable<HtmlNode> Content = Document.Descendants(0).Where(n => n.HasClass(""));
+            foreach (HtmlNode content in Content)
+            {
+                var src = content.ChildNodes[1].GetAttributeValue("src", "none");
+                var name = content.ChildNodes[3].InnerText;
+                var description = content.ChildNodes[5].InnerText;
+                Console.WriteLine(name + "\n" + "Изображение: " + src + "\n" + "Описание: " + description);
+            }
+        }
+
+        public static string GetHtmlFromUrl(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string htmlCode = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return htmlCode;
         }
     }
 }
