ee79b01b30ff96801a6225031a7c81c9548be702
kirillColeBou <kirillteplycov888@gmail.com>
2024-12-13 18:59:55 +0500

    Получение информации с сайта. Тепляков

--- pr7_Тепляков/Program.cs
+++ pr7_Тепляков/Program.cs
@@ -12,6 +12,17 @@ namespace pr7_Тепляков
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create("");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string data = reader.ReadToEnd();
+            Console.WriteLine(data);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
